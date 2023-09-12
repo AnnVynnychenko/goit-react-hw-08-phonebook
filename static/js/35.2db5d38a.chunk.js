69f453c566002ff2a15b1ccfc61653312434265c
AnnVynnychenko <121746772+AnnVynnychenko@users.noreply.github.com>
2023-09-12 16:59:12 +0000

    Deploying to gh-pages from @ AnnVynnychenko/goit-react-hw-08-phonebook@2df90b9eefd64f3b5ad0589f125a7a3265f6cdcb 🚀

---
+++ static/js/35.2db5d38a.chunk.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkreact_homework_template=self.webpackChunkreact_homework_template||[]).push([[35],{35:function(e,t,r){r.r(t),r.d(t,{default:function(){return o}});var n=r(184),o=function(){return(0,n.jsx)("div",{children:"Your Phone Book"})}}}]);
+//# sourceMappingURL=35.2db5d38a.chunk.js.map
